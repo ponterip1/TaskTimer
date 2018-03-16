c72cb0f43827cbfc6a6ba71ee41cc935c6f1a527
ponterip1 <pjp08@yahoo.com>
2018-03-15 20:35:26 -0700

    Added main menu options to user interface

--- app/src/main/java/com/peterponterio/tasktimer/MainActivity.java
+++ app/src/main/java/com/peterponterio/tasktimer/MainActivity.java
@@ -190,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menumain_settings) {
             return true;
         }
 
