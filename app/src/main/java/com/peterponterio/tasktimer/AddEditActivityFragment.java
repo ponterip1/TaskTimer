cb55a0a978e08f4a789b1c28e7046fc1ca42cb23
ponterip1 <pjp08@yahoo.com>
2018-03-16 16:46:52 -0700

    Added functionality to the add/edit button so data can be saved to the database

--- app/src/main/java/com/peterponterio/tasktimer/AddEditActivityFragment.java
+++ app/src/main/java/com/peterponterio/tasktimer/AddEditActivityFragment.java
@@ -1,22 +1,184 @@
 package com.peterponterio.tasktimer;
 
-import android.support.v4.app.Fragment;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AddEditActivityFragment extends Fragment {
+    private static final String TAG = "AddEditActivityFragment";
+
+
+    //enum used to keep track of whether fragments being used to add or edit
+    public enum FragmentEditMode { EDIT, ADD }
+    private FragmentEditMode mMode; //private field to track fragment
+
+
+    /*
+        Get references to all our widgets so we can attach a listener to the button and get the text
+        type into the edit tasks
+     */
+    private EditText mNameTextView;
+    private EditText mDescriptionTextView;
+    private EditText mSortOrderTextView;
+    private Button mSaveButton;
+
 
     public AddEditActivityFragment() {
+        Log.d(TAG, "AddEditActivityFragment: constructor called");
     }
 
+
+    //Fragments onCreateView method returns the view it inflates
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_edit, container, false);
+        Log.d(TAG, "onCreateView: starts");
+
+        //store the inflated view
+        View view = inflater.inflate(R.layout.fragment_add_edit, container, false);
+
+        /*
+            used the view object we created from the inflater to extract out the references to the
+            various widgets on screen and we're saving those in our fields so that we can reference them
+            elsewhere. Then were returning the view
+         */
+        mNameTextView = (EditText) view.findViewById(R.id.addedit_name);
+        mDescriptionTextView = (EditText) view.findViewById(R.id.addedit_description);
+        mSortOrderTextView = (EditText) view.findViewById(R.id.addedit_sortorder);
+        mSaveButton = (Button) view.findViewById(R.id.addedit_save);
+
+
+
+
+        /*
+            when we pass a task object in the intent, its passed as a bundle. getExtras() is getting
+            the extra arguments that were included in the intent that started the activity that the
+            fragment is associated with. When mainActivity wants to add a new task, it doesnt provide
+            any extras to the intent, so in this case arguments will be null. Even if there are arguments,
+            theres no guarantee that they contain a task.
+
+            We use the getSerializable method and pass the task.class.getSimpleName() as the key. If that
+            doesnt return null, we have a task to edit. We can then initialize the contents of our editText
+            widgets with the task details. If it was null, it means we didnt get a task to edit, so we're
+            just setting mMode to ADD
+         */
+        Bundle arguments = getActivity().getIntent().getExtras();
+
+        /*
+            task is made final because we're going to refer to it in the buttons onClickListener, and
+            an inner class can only access final variables of its enclosing class. That means we have
+            to initialize it to null, otherwise theres a path through the code that could result in it
+            not being initialized, which results in an error
+         */
+        final Task task;
+        if(arguments != null) {
+            Log.d(TAG, "onCreateView: retrieving task details");
+
+            task = (Task) arguments.getSerializable(Task.class.getSimpleName());
+            if(task != null) {
+                Log.d(TAG, "onCreateView: Task details found, editing...");
+                mNameTextView.setText(task.getName());
+                mDescriptionTextView.setText(task.getDescription());
+                mSortOrderTextView.setText(Integer.toString(task.getSortOrder()));
+                mMode = FragmentEditMode.EDIT;
+            } else {
+                //No task, so we must be adding a new task, and not editing an existing one
+                mMode = FragmentEditMode.ADD;
+            }
+        } else {
+            task = null;
+            Log.d(TAG, "onCreateView: No arguments, adding new record");
+            mMode = FragmentEditMode.ADD;
+        }
+
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //update the database if at least one field has changed
+                //no need to hit the database unless this has happened
+
+                /*
+                    converted to an int right at the start to save time, rather than duplicating the
+                    conversion code
+                 */
+                int so;
+                if(mSortOrderTextView.length()>0) {
+                    so = Integer.parseInt(mSortOrderTextView.getText().toString());
+                } else {
+                    so = 0;
+                }
+
+
+                /*
+                    get a reference to the activity that the fragments attached to first, and then
+                    call getContentResolver on that activity reference
+                 */
+                ContentResolver contentResolver = getActivity().getContentResolver();
+                ContentValues values = new ContentValues();
+
+
+                /*
+                    if the user clicks the save button without making any changes to the data, then
+                     theres no point accessing the database to update a record with the same values.
+                     this is really important on an android device because we dont want to be updating the
+                     database or doing any processing of any significance unless we really need to
+
+                     So in the case of an EDIT, were checking each field and in the case of an ADD, were
+                     adding a new record. we just make sure the name field isnt blank as thats the
+                     only column that requires a value on the table.(description and sortorder are optional)
+
+                 */
+                switch (mMode) {
+                    case EDIT:
+                        /*
+                            checking each editText value against the orignal task object that was passed
+                            in the bundle. and if the value has changed, the new value is added to the
+                             values ContentValues and once all the fields have been checked and if theres
+                             anything in values, the data is saved by calling the update method of the
+                             content resolver
+                         */
+                        if(!mNameTextView.getText().toString().equals(task.getName())) {
+                            values.put(TasksContract.Columns.TASKS_NAME, mNameTextView.getText().toString());
+                        }
+                        if(!mDescriptionTextView.getText().toString().equals(task.getDescription())) {
+                            values.put(TasksContract.Columns.TASKS_DESCRIPTION, mNameTextView.getText().toString());
+                        }
+                        if(so != task.getSortOrder()) {
+                            values.put(TasksContract.Columns.TASKS_SORTORDER, so);
+                        }
+                        if(values.size() != 0) {
+                            Log.d(TAG, "onClick: updating task");
+                            contentResolver.update(TasksContract.buildTaskUri(task.getId()), values, null, null);
+                        }
+                        break;
+                    case ADD:
+                        if(mNameTextView.length()>0) {
+                            Log.d(TAG, "onClick: adding new task");
+                            values.put(TasksContract.Columns.TASKS_NAME, mNameTextView.getText().toString());
+                            values.put(TasksContract.Columns.TASKS_DESCRIPTION, mDescriptionTextView.getText().toString());
+                            values.put(TasksContract.Columns.TASKS_SORTORDER, so);
+                            contentResolver.insert(TasksContract.CONTENT_URI, values);
+                        }
+                        break;
+                }
+                Log.d(TAG, "onClick: Done editing");
+            }
+        });
+        Log.d(TAG, "onCreateView: Exiting...");
+
+        return view;
+
+
     }
 }
--- app/src/main/java/com/peterponterio/tasktimer/MainActivity.java
+++ app/src/main/java/com/peterponterio/tasktimer/MainActivity.java
@@ -1,20 +1,23 @@
 package com.peterponterio.tasktimer;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.database.Cursor;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+
+    //whether or not the activity is in 2-pane mode
+    //ex: running in landscape mode on a tablet
+    private boolean mTwoPane = false;
+
+
+    private static final String ADD_EDIT_FRAGEMENT = "AddEditFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,156 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*
-             specify the columns we want to see in the query by creating a string array listing them
 
-             we then get a reference to the devices content resolver and call its query method and because
-             were using the uri for our tasks table, the content resolver knows that it has to extract
-             the authority from the uri to work out which provider to use and then it passes the entire
-             uri onto our provider to run the query
-
-             get a cursor from the content resolver and once weve got it were going to loop through it
-            logging all the columns in the cursor, as long as the cursor isnt null
-         */
-        String[] projection = { TasksContract.Columns._ID,
-                                TasksContract.Columns.TASKS_NAME,
-                                TasksContract.Columns.TASKS_DESCRIPTION,
-                                TasksContract.Columns.TASKS_SORTORDER};
-        ContentResolver contentResolver = getContentResolver();
-
-
-        ContentValues values = new ContentValues();
-
-        /*
-            Inserting
-
-            The ContentValues object is used to provide the new values that we're going to insert into
-             the database. A ContentValues object is very similar to a bundle. We're using its put method
-             to store key value pairs where the key is the column name in the table that we're inserting
-             into. We have to specify a value for the name column because it was set as not null when
-             we created the tasks table
-         */
-//        values.put(TasksContract.Columns.TASKS_NAME, "New Task 1");
-//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Description 1");
-//        values.put(TasksContract.Columns.TASKS_SORTORDER, 2);
-//        Uri uri = contentResolver.insert(TasksContract.CONTENT_URI, values);
-
-
-        /*
-            Updating
-
-            the update method needs a Uri with a set of values. We're passing the values, and we've
-             also included the Uri that includes the id by calling the buildTaskUri method, passing the
-             id = 4 in this case, for the record that we want to be updated, and null for the other
-             two parameters
-         */
-//        values.put(TasksContract.Columns.TASKS_NAME, "Content Provider");
-//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Record content provider video");
-//        int count = contentResolver.update(TasksContract.buildTaskUri(4), values, null, null);
-//        Log.d(TAG, "onCreate: " + count + " record(s) updated");
-
-
-
-
-        /*
-            Updating using selection
-
-            looking for all values in the database with a tasks sort order of 2
-            The WHERE clause is the selection and the selectionArgs is null. So weve added a WHERE
-            clause to update all entries in the database that have got columns.Task_SortOrder with a
-            current value of two
-         */
-//        values.put(TasksContract.Columns.TASKS_SORTORDER, "99");
-//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Completed");
-//        String selection = TasksContract.Columns.TASKS_SORTORDER + " = " + 2;
-//        int count = contentResolver.update(TasksContract.CONTENT_URI, values, selection, null);
-//        Log.d(TAG, "onCreate: " + count + " record(s) updated");
-
-
-
-        /*
-            Updating using the selectionArgs.
-
-            we're providing a single arg value, 99. When the sql statements built up to perform the
-            update, each of the values in args is used to replace the question marks in the selection.
-            Theyre replaced in order. For this example theres only 1, which is 99, but if you had three
-            or four, they would be replaced in order that they appear in the array
-
-            If all the values for the criteria come from inside your code, then it doesnt really matter
-            which method to use
-         */
-//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "For deletion");
-//        String selection = TasksContract.Columns.TASKS_SORTORDER + " = ?";
-//        String[] args = {"99"};
-//        int count = contentResolver.update(TasksContract.CONTENT_URI, values, selection, args);
-//        Log.d(TAG, "onCreate: " + count + " record(s) updated");
-
-
-
-        /*
-            Deleting
-
-            pass the value of 3, which is the record id we want to delete, no where clause, no selectionArgs
-         */
-//        int count = contentResolver.delete(TasksContract.buildTaskUri(3), null, null);
-//        Log.d(TAG, "onCreate: " + count + " record(s) deleted");
-
-
-
-        /*
-            Deleting using sectionArgs
-
-            passing taskcontract.CONTENT_URI. The argument we're looking for are TASKS_DESCRIPTIONS
-            where the value is set to "For deletion"
-         */
-//        String selection = TasksContract.Columns.TASKS_DESCRIPTION + " = ?";
-//        String[] args = {"For deletion"};
-//        int count = contentResolver.delete(TasksContract.CONTENT_URI, selection, args);
-//        Log.d(TAG, "onCreate: " + count + " record(s) deleted");
-
-
-
-
-
-
-        Cursor cursor = contentResolver.query(TasksContract.CONTENT_URI,
-                projection,
-                null,
-                null,
-                TasksContract.Columns.TASKS_SORTORDER);
-
-        /*
-            instead of specifying which columns to log, the code just loops through all the columns in
-            the cursor and logs the name and value of each one, as well as getting the value of a column
-            using the getString method. we can also get the name of the column using the getColumnName
-            method
-         */
-        if(cursor != null){
-            Log.d(TAG, "onCreate: number of rows: " + cursor.getCount());
-            while (cursor.moveToNext()) {
-                for(int i=0; i<cursor.getColumnCount(); i++) {
-                    Log.d(TAG, "onCreate: " + cursor.getColumnName(i) + ": " + cursor.getString(i));
-                }
-                Log.d(TAG, "onCreate: ==================================");
-            }
-            cursor.close();
-        }
-
-//        AppDatabase appDatabase = AppDatabase.getInstance(this);
-//        final SQLiteDatabase db = appDatabase.getReadableDatabase();
-        
-        
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -189,11 +43,50 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menumain_settings) {
-            return true;
+        //check which menu item is tapped
+        switch (id) {
+            case R.id.menumain_addTask:
+                taskEditRequest(null);
+                break;
+            case R.id.menumain_showDurations:
+                break;
+            case R.id.menumain_settings:
+                break;
+            case R.id.menumain_showAbout:
+                break;
+            case R.id.menumain_generate:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /*
+        start of by seeing if the app should be in two pane mode. If the apps running on a phone in
+        portrait mode, we start the AddEditActivity using an intent. If our taskEditRequest method's
+        called with a task argument that isnt null, then that indicates that an existing task is to be
+        edited. but if the task is null, then we're adding a new task. So if we have a task to edit,
+         we add it to the intent by calling the putExtra method
+     */
+    private void taskEditRequest(Task task) {
+        Log.d(TAG, "taskEditRequest: starts");
+
+        if (mTwoPane) {
+            Log.d(TAG, "taskEditRequest: in two-pane mode (tablet");
+        } else {
+            Log.d(TAG, "taskEditRequest: in single-pane mode (phone");
+
+            //in single pane mode, start the detaul activity for the selected item Id
+            Intent detailIntent = new Intent(this, AddEditActivity.class);
+            if (task != null) { //editing a task
+                detailIntent.putExtra(Task.class.getSimpleName(), task);
+                startActivity(detailIntent);
+            } else { //adding a new task
+                startActivity(detailIntent);
+            }
+        }
+
+
     }
 }
